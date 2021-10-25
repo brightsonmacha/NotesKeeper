21fe62efc0e17d03dfdb6c23f848d6d628897b6f
ngugikubai <kubaibrian@gmail.com>
2021-10-25 20:38:31 +0300

    Creating Note title and text

---
+++ app/src/main/java/com/example/noteskeeper/NoteListActivity.kt
@@ -0,0 +1,33 @@
+package com.example.noteskeeper
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.noteskeeper.databinding.ActivityNoteListBinding
+
+class NoteListActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+
+}
